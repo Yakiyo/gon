5fb26b0173af31253a07021a2c84e4a61e7ccf7a
Yakiyo <yakiyo1206@gmail.com>
2023-09-06 05:07:15 +0000

    chore: silent usage and errors on all commands

--- cmd/install.go
+++ cmd/install.go
@@ -16,6 +16,8 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
+	SilenceUsage:  true,
+	SilenceErrors: true,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("install called")
 	},
