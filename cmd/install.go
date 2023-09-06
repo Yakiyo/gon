e13d1aa342dda16ca272c77cd94ba804dea4329b
Yakiyo <yakiyo1206@gmail.com>
2023-09-06 10:46:29 +0000

    feat: resolve versions

--- cmd/install.go
+++ cmd/install.go
@@ -2,37 +2,73 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
+	"github.com/Yakiyo/gom/utils"
+	"github.com/Yakiyo/gom/versions"
+	"github.com/samber/lo"
 	"github.com/spf13/cobra"
 )
 
 // installCmd represents the install command
 var installCmd = &cobra.Command{
 	Use:   "install",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Install a version of the Go compiler",
+	Long: `Install a version of the Go compiler
+	
+If no argument is provided, it tries to find and use the version from a go.mod file in the current directory.
+"latest" or "lts" can also be given as argument to install the latest available stable version.
+Otherwise it expects a valid semver compliant string as argument
+`,
 	SilenceUsage:  true,
 	SilenceErrors: true,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("install called")
+	Example: "gom install lts    # install latest version\n" +
+		"gom install 1.20.1 # install specific version\n" +
+		"gom install        # use a go.mod file in current directory",
+	Args: cobra.MaximumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var version string
+		if len(args) == 1 {
+			version = args[0]
+			vers, err := versions.List()
+			if err != nil {
+				return err
+			}
+			if version == "latest" || version == "lts" {
+				version = vers[0]
+			}
+			version = strings.TrimSuffix(strings.ToLower(version), "v")
+			if !lo.Contains(vers, version) {
+				return fmt.Errorf("Invalid version, %v is not a valid version for Go", version)
+			}
+		} else {
+			if !utils.PathExists("./go.mod") {
+				return fmt.Errorf("No version provided and current directory does not contain a go.mod file. Provide a version explicitly.")
+			}
+			b, err := os.ReadFile("./go.mod")
+			if err != nil {
+				return fmt.Errorf("Unable to read local go.mod file, received error %s", err)
+			}
+			str := string(b)
+			// find line that contains `go {{ version }}`
+			vline, ok := lo.Find[string](strings.Split(str, "\n"), func(item string) bool {
+				return strings.HasPrefix(item, "go")
+			})
+			if !ok {
+				return fmt.Errorf("Invalid go.mod file, file does not specify go version")
+			}
+			version = strings.TrimSpace(strings.TrimPrefix(vline, "go "))
+			if version == "" {
+				return fmt.Errorf("Could not parse version from go.mod file. Line contains %s", vline)
+			}
+		}
+
+		fmt.Println(version)
+		return nil
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(installCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// installCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// installCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
