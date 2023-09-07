fd9c72d37c8652e9f06b1beb29ed240bd9c6f4ac
Yakiyo <yakiyo1206@gmail.com>
2023-09-07 19:30:25 +0000

    feat: better path command

--- cmd/path.go
+++ cmd/path.go
@@ -3,45 +3,45 @@ package cmd
 import (
 	"fmt"
 	"path/filepath"
+	"strings"
 
-	"github.com/Yakiyo/gon/utils"
 	"github.com/Yakiyo/gon/utils/where"
+	"github.com/samber/lo"
 	"github.com/spf13/cobra"
 )
 
+var validArgs = []string{"bin", "current", "root"}
+
 // pathCmd represents the path command
 var pathCmd = &cobra.Command{
-	Use:   "path --bin-dir|--current|--root",
-	Short: "Show root path used by gon",
-	Long: `Show root path used by gon.
+	Use:   "path bin|current|root",
+	Short: "Show paths used by gon",
+	Long: `Show paths used by gon.
 	
-This is handy to automatically add the path to your shell's env`,
+This can be used to easily add the required path(s) to a shell's env/sys PATH
+bin     - Shows the path to the directory where active Go executable is situated
+current - Shows the path to the directory of the active Go version
+root    - Shows the root directory used by gon`,
 	SilenceErrors: true,
 	SilenceUsage:  true,
+	Args:          cobra.ExactArgs(1),
+	ValidArgs:     validArgs,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		arg := strings.ToLower(args[0])
+		if !lo.Contains(validArgs, arg) {
+			return fmt.Errorf("Missing required argument. Must be one of %v", strings.Join(validArgs, ", "))
+		}
+		if arg == "bin" {
+			fmt.Println(filepath.Join(where.Bin(), "bin"))
+		} else if arg == "current" {
+			fmt.Println(where.Bin())
+		} else if arg == "root" {
+			fmt.Println(where.RootDir())
+		}
+		return nil
+	},
 }
 
 func init() {
-	f := pathCmd.Flags()
-	f.Bool("bin-dir", false, "Show the bin directory, within which is the go executable")
-	f.Bool("current", false, "Show the current directory, within which the currently used version is stored")
-	f.Bool("root", false, "Show gon root dir, the directory used by gon")
 	rootCmd.AddCommand(pathCmd)
-
-	// need to define it separately, else it creates a cycle with pathCmd
-	pathCmd.RunE = func(cmd *cobra.Command, args []string) error {
-		if used("bin-dir") {
-			fmt.Println(filepath.Join(where.Bin(), "bin"))
-		} else if used("current") {
-			fmt.Println(where.Bin())
-		} else if used("root") {
-			fmt.Println(where.RootDir())
-		} else {
-			return fmt.Errorf("Must use one of bin, current or root flag for this command")
-		}
-		return nil
-	}
-}
-
-func used(flag string) bool {
-	return utils.Must(pathCmd.Flags().GetBool(flag))
 }
