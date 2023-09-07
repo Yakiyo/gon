5867853cc9afad557e125e910e8268402cab7155
Yakiyo <yakiyo1206@gmail.com>
2023-09-07 15:08:51 +0000

    feat: current command

--- cmd/current.go
+++ cmd/current.go
@@ -2,37 +2,44 @@ package cmd
 
 import (
 	"fmt"
+	"os/exec"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
 // currentCmd represents the current command
 var currentCmd = &cobra.Command{
-	Use:   "current",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:           "current",
+	Short:         "View currently active Go version",
+	Args:          cobra.NoArgs,
 	SilenceUsage:  true,
 	SilenceErrors: true,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("current called")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		_, err := exec.LookPath("go")
+		if err != nil {
+			fmt.Println("No version of Go is currently active")
+			return nil
+		}
+		out, err := exec.Command("go", "version").Output()
+		if err != nil {
+			return anyhow("Unable to run Go from command line", err)
+		}
+		// usually output of `go version` is as follows
+		// "go version go{{version}} {{platform}}/{{architecture}}"
+		// so it's a 4 word string, so we slice and dice em to get the version
+		output := strings.Split(string(out), " ")
+		if len(output) < 4 {
+			// unexpected output format, so just return it
+			fmt.Println(strings.Join(output, " "))
+			return nil
+		}
+		version := "v" + strings.TrimPrefix(output[2], "go")
+		fmt.Println(version)
+		return nil
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(currentCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// currentCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// currentCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
