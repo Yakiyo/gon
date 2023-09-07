5a2359f1f1abe6ab1914d5bc2f258e738b942841
Yakiyo <yakiyo1206@gmail.com>
2023-09-07 19:06:18 +0000

    refactor: move out logic to separate func

--- cmd/current.go
+++ cmd/current.go
@@ -16,30 +16,40 @@ var currentCmd = &cobra.Command{
 	SilenceUsage:  true,
 	SilenceErrors: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		_, err := exec.LookPath("go")
+		version, err := getCurrent()
 		if err != nil {
-			fmt.Println("No version of Go is currently active")
+			return err
+		}
+		if version == "" {
+			fmt.Println("No version currently active")
 			return nil
 		}
-		out, err := exec.Command("go", "version").Output()
-		if err != nil {
-			return anyhow("Unable to run Go from command line", err)
-		}
-		// usually output of `go version` is as follows
-		// "go version go{{version}} {{platform}}/{{architecture}}"
-		// so it's a 4 word string, so we slice and dice em to get the version
-		output := strings.Split(string(out), " ")
-		if len(output) < 4 {
-			// unexpected output format, so just return it
-			fmt.Println(strings.Join(output, " "))
-			return nil
-		}
-		version := "v" + strings.TrimPrefix(output[2], "go")
-		fmt.Println(version)
+		fmt.Println("v" + version)
 		return nil
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(currentCmd)
+}
+
+func getCurrent() (string, error) {
+	_, err := exec.LookPath("go")
+	if err != nil {
+		return "", nil
+	}
+	out, err := exec.Command("go", "version").Output()
+	if err != nil {
+		return "", anyhow("Unable to run Go from command line", err)
+	}
+	// usually output of `go version` is as follows
+	// "go version go{{version}} {{platform}}/{{architecture}}"
+	// so it's a 4 word string, so we slice and dice em to get the version
+	output := strings.Split(string(out), " ")
+	if len(output) < 4 {
+		// unexpected output format, so just return it
+		return strings.Join(output, " "), nil
+	}
+	version := strings.TrimPrefix(output[2], "go")
+	return version, nil
 }
