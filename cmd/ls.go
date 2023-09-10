e2bc604facce899007db48109146a3a15ad5f96d
Yakiyo <yakiyo1206@gmail.com>
2023-09-10 10:39:10 +0000

    refactor: move list generation to separate func

--- cmd/ls.go
+++ cmd/ls.go
@@ -24,21 +24,10 @@ var lsCmd = &cobra.Command{
 	SilenceErrors: true,
 	Args:          cobra.NoArgs,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		intls := where.Installations()
-		if !utils.PathExists(intls) {
-			fmt.Println("No versions installed")
-			return nil
-		}
-		dirs, err := os.ReadDir(intls)
+		vers, err := listLocals()
 		if err != nil {
-			return fmt.Errorf("Unable to read dir entries for installations due to error %v", err)
+			return err
 		}
-		vers := lo.FilterMap[fs.DirEntry, string](dirs, func(f fs.DirEntry, _ int) (string, bool) {
-			if !f.IsDir() || strings.HasPrefix(f.Name(), ".") {
-				return "", false
-			}
-			return filepath.Base(f.Name()), true
-		})
 		if len(vers) < 1 {
 			fmt.Println("No versions installed")
 			return nil
@@ -86,4 +75,23 @@ func flipMap(m map[string]string) map[string][]string {
 		res[v] = old
 	}
 	return res
+}
+
+// list locally installed versions
+func listLocals() ([]string, error) {
+	intls := where.Installations()
+	if !utils.PathExists(intls) {
+		return []string{}, nil
+	}
+	dirs, err := os.ReadDir(intls)
+	if err != nil {
+		return []string{}, fmt.Errorf("Unable to read dir entries for installations due to error %v", err)
+	}
+	vers := lo.FilterMap[fs.DirEntry, string](dirs, func(f fs.DirEntry, _ int) (string, bool) {
+		if !f.IsDir() || strings.HasPrefix(f.Name(), ".") {
+			return "", false
+		}
+		return filepath.Base(f.Name()), true
+	})
+	return vers, nil
 }
